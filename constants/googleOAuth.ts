171d6bb1b0ff057236f3807145ad4f413753de4f
Sandigas <sandigasjelord@gmail.com>
2026-07-01 18:49:41 +0800

    Delete constants directory

--- constants/googleOAuth.ts
+++
@@ -1,3 +0,0 @@
-/** Web Client ID from Firebase → Authentication → Google → Web SDK configuration */
-export const googleWebClientId =
-  '184755804059-tna0gg12b8ufkod95vfeqllbqfaqtdm3.apps.googleusercontent.com';
